44415050590038457d93b14d51a053de95182859
lastbot1 <109231043+lastbot1@users.noreply.github.com>
2022-08-05 19:09:45 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,14 +12,14 @@ const chalk = require('chalk')
 global.zenzkey = '06d483bbc651' //Register Atau Beli Di zenzapis.xyz
 
 //===> SESUAIKAN DENGAN KEBUTUHAN MU
-global.ownerNumber = ['6285609233482','6285609233482'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
-global.Rowner = '6285609233482' //Owner Utama
-global.ownerName = 'Nzrl Afndi' //Ganti Jadi Namamu
-global.packname = 'Dracule Mihawk' //Ganti Jadi Nama Botmu
-global.author = 'By NzrlAfndi' //Ganti Jadi Namamu
+global.ownerNumber = ['6281249192779','6287819353757'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
+global.Rowner = '6287819353757' //Owner Utama
+global.ownerName = 'Rizz' //Ganti Jadi Namamu
+global.packname = 'Alizmy boy' //Ganti Jadi Nama Botmu
+global.author = 'By Rizz' //Ganti Jadi Namamu
 global.prefa = ['','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi' //Saran Gausah Diganti Lagi
-global.webme = 'https://linktr.ee/NzrlAfndi' //Ganti Jadi Bebas Web Sosmed Mu
+global.webme = 'https://msha.ke/alizmystore/' //Ganti Jadi Bebas Web Sosmed Mu
 
 //====> SARAN GAUSAH DIGANTI SIH DAH PAS SEMUA
 global.limitAwal = {
